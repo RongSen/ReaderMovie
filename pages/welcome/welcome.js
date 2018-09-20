c4b636b2da8db01d6915ddc76a8b46d43a9a5e05
RongSen <610567970@qq.com>
2018-09-21 01:29:22 +0800

    完善

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,3 +1,19 @@
 Page({
-
+  onTap: function(event){
+    wx.redirectTo({
+      url: '../posts/post',
+      success: function(res){
+        console.log('redirectTo success');
+      },
+      fail: function(){
+        console.log('redirectTo fail');
+      },
+      complete: function(){
+        console.log('redirectTo comlete');
+      }
+    });
+  },
+  onHide: function(){
+    console.log('welcome is hided');
+  }
 })
